40470ac467f60381cbe0c5fd9c5508ea0f778891
Jong Park <parkj6@oregonstate.edu>
2019-01-22 22:17:59 -0800

    I did it.

--- hw1/assn1.m
+++ hw1/assn1.m
@@ -4,19 +4,19 @@
 
 
 %% Initial Plot
-x = -2: 0.02: 2;
-y = -2: 0.02: 2;
-plot(x, zeros(size(x)), 'b', zeros(size(y)),y,'b')
-axis([-2, 2, -2, 2])
-title("Plot of f(x) = x + e^{-Bx^2}")
-xlabel('x')
-ylabel('y')
-hold on 
+x = -2: 0.01: 2;
+y = -2: 0.01: 2;
+plot(x, zeros(size(x)), 'b', zeros(size(y)),y,'b');
+axis([-2, 2, -2, 2]);
+title("Plot of f(x) = x + e^{-Bx^2}");
+xlabel('x');
+ylabel('y');
+hold on;         % to what you got
 
 
 %% Part a
-B = [1, 5, 10, 25, 50];   % Initial input
-x = -2: 0.01: 2;          % Used for making pretty graphs
+B = [1, 5, 10, 25, 50];                  % Initial input
+x = -2: 0.01: 2;                         % Used for making pretty graphs
 
 for k = 1 : length(B)
     bval = B(k);
@@ -25,21 +25,57 @@ for k = 1 : length(B)
 end
 
 %% Part b
-x = [-0.60, -0.40, -0.30, -0.25, -0.20];    % These are actually x0
+x = [-0.60, -0.40, -0.30, -0.25, -0.20]; % These are actually x0
 max_iter = 20;
 f =      'x + exp(-B .* (x .^ 2))';
 fprime = '1 + 2.*(-B) .* exp(-B .* (x .^ 2)) .* x';
-%     iters(k) = xval;
-iters = x;
-for j = 1 : max_iter       % 20 by default
-    xnew = x - eval(f) ./ eval(fprime);
-    iters = [iters; xnew];
-    test = abs(xnew -x) ./ (abs(xnew)+eps);
+iters = [0,x];                           % Start at iteration 0
+for j = 1 : max_iter                     % Max 20 iterations
+    xgood = x - eval(f) ./ eval(fprime);
+    iters = [iters; j, xgood];            % Added iteration #
+    test = abs(xgood -x) ./ (abs(xgood)+eps);
     if max(test) < 10*eps, break, end
-    x = xnew;
+    x = xgood;
 end
 
-%% Changing the Format to long
-format long;
+format long;                             % Changing the format to long
+iters;                                   % Display iterations.
+
+%% Part c
+for k = 1 : length(B)
+    figure                               % Used to make individual charts
+    x = -2: 0.01: 2;         
+    y = -2: 0.01: 2;
+    plot(x, zeros(size(x)), 'b', zeros(size(y)),y,'b')
+    axis([-2, 2, -2, 2])
+    title(sprintf("Plot of f(x) = x + e^{- %d x^2}",B(k)))
+    xlabel('x')
+    ylabel('y')
+    hold on     
+ 
+    y = x + exp(-B(k) .* (x .^ 2));      % Individual graphs
+    plot (x,y);
+    
+    x = -2: 0.01: 2;
+    m = 1 + 2.*(-B(k)) .* exp(-B(k) .* (xgood(k) .^ 2)) .* xgood(k); % slope
+    y = m .* x - m .* xgood(k);           % Graph of the new slope
+    plot (x,y)
+    hold off
+end
+
+x = [0 0 0 0 0];                         % All x0 = 0;
+max_iter = 20;
+f =      'x + exp(-B .* (x .^ 2))';
+fprime = '1 + 2.*(-B) .* exp(-B .* (x .^ 2)) .* x';
+iters = [0,x];                           % Start at iteration 0
+for j = 1 : max_iter                     % Max 20 iterations
+    xbad = x - eval(f) ./ eval(fprime);
+    iters = [iters; j, xbad];           % Added iteration #
+    test = abs(xbad -x) ./ (abs(xbad)+eps);
+    if max(test) < 10*eps, break, end
+    x = xbad;
+end
+
+%% Changing the Format to short;
+format short 
 iters
-% iterall
