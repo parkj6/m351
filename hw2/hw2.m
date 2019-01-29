5b1d91e844cddaaecf14e64c00eb005b20ca481d
Jong Park <parkj6@oregonstate.edu>
2019-01-29 14:55:52 -0800

    newton's method added

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -4,8 +4,24 @@
 
 %% 1. part a
 b = 0.1 : 0.1 :10;
-x = log(b)
-y = exp(b)
+x = 0;
+y = exp(x)
+
+f = 'log(x)';		% string variable
+fprime = '1/x';		% string variable
+maxiterations = 20;
+
+x = [0, 1.5, 3];				% Vector of starting values
+iters = x;						% Used to accumulate iterations
+for k = 1 : maxiterations       % 20 by default
+	xnew = x - eval(f) ./ eval(fprime);
+	iters = [iters; xnew];
+	test = abs(xnew -x) ./ (abs(xnew)+eps);
+	if max(test) < 10*eps, break, end
+	x = xnew;
+end
+
+
 plot (x, y, x, zeros(size(x)), zeros(size(y)), y);
 
 
@@ -25,4 +41,4 @@ n=21;           % The computed roots aren't even real.
 
 p = poly(1:n);  % p Contains coefficients (in descending order of a polynomial
                 % whose roots are components of the vector 1:n
-r = roots(p) 	% r contains the computed roots of this polynomial.
+r = roots(p); 	% r contains the computed roots of this polynomial.
