fdba7cf41ab0f638b0075aa934473ea0721946ef
Jong Park <parkj6@oregonstate.edu>
2019-02-27 01:22:24 -0800

    hw3 is getting somewhere

--- hw3/hw3.m
+++ hw3/hw3.m
@@ -1,65 +1,67 @@
 %% Jong Park (parkj6)
-% Math 351 - Numerical Analysis HW 1
+% Math 351 - Numerical Analysis 
+% HW 3
 
 %% 1
 
 % choose a spacing that is fine enough that the curve appears smooth.
 x = linspace(-1,1);
+%n = 2;
+n = 10;
 
 %f(x) = e^x for [-1,1]
-y = exp(x);
+y1 = exp(x);
+
+%Chebyshev Polynomials
+c = chebyshevT(n,x);
 
 % if Vectors x and y each have n+1 components,
-n1 = 2;
-n2 = 10;
-coeffs = polyfit (x,y,n1); 
+coeffs = polyfit (x,y1,n); 
+cpoly  = polyfit (x,c, n);
 % produces the coefficients of polynomial of degree n or less that interpolates
 % points having horizontal coordinates in vector and vertical coordiantes in
 % vector y. coeffs are listed in order of descending powers of x.
 
 % to compute the values of that polynomial:
 polyplot = polyval (coeffs, x); 
+cplot = polyval (cpoly, x);
 % produces values of the polynomial at horizontal coordinates
-% specified in the vector xplot.
+% specified in the vector x.
 
 % compute areas where f and p meets.
-xroots = polyxpoly(x,y, x,polyplot);
-
-for i=1: size(xroots)
-    yroots = exp(xroots);
-end
+[xr,yr] = polyxpoly(x,y1, x,polyplot);              % x-root
+[cxr,yxr] = polyxpoly(x,c, x,cplot);                  
 
 % f-p
-fp = abs (y - polyplot);
+fp = abs (y1 - polyplot);
+fpc = abs (c - cplot);
 
-%Chebyshev Polynomials
-c1 = chebyshevT(n1,x);
-c2 = chebyshevU(n2,x);
 
 %% Subplots (n = 2)
 % F as solid curve
 % P as dashed curve
 % F==P - Small circle 
-subplot (221); plot (x,y, '-', x, polyplot,'--', xroots, yroots, 'o');
-legend ('f(x) = e^x', 'p(x)', 'f(x) == p(x)', 'location','best')
+subplot (221); plot (x,y1, '-', x, polyplot,'--', xr, yr, 'o');
+legend ('f(x) = e^x', 'p(x)', 'f(x) == p(x)', 'location','north')
+title ('f(x) = e^x, evenly spaced (n=10)')
 
 % Same thing with Chebyshev points
-subplot (222); plot (c1, '-');
+subplot (222); plot (x,c, '-', x, cplot,'--',cxr,yxr,'o');
+legend ('f(x) = T_10(x)', 'p(x)', 'f(x) == p(x)', 'location','north')
+title ('Chebyshev Polynomial')
 
 % Error f-p (Normal)
 subplot (223); plot (x, fp);
 legend('f(x) - p(x)','location','best')
 
 % Error f-p (Chebyshev)
-subplot (224); %plot (x,y);
-
-
-
-
+subplot (224); plot (x,fpc);
+legend('T_n(x) - p(x)','location','best')
 
 
 %% 2
-
+% f(x) = 1 / (1+ 25x^2)
+y2 = 1 ./ (1 + 25 * x.^2);
 
 
 
