ab4e8d61084e80eacc2b7a80849f01e94e19446d
Jong Park <parkj6@oregonstate.edu>
2019-03-04 19:59:38 -0800

    hw4 started

---
+++ hw4/hw4.m
@@ -0,0 +1,25 @@
+%% Jong Park (parkj6)
+% Math 351 - Numerical Analysis 
+% HW 4 - Data Fitting
+
+%% Given
+y = [1, 0.5, 0.4, 0.25, 0.1, 0.1, 0.05, 0.02, 0.01];
+m = length(y);
+x = 1:m;
+
+%plot (x,y)
+
+%% 1.p(x) = c_1e^(-0.5x) + c_2e^(-x), c1, c2 are constants. 
+%% a)  Use appx relation to derive an overdetermined system
+
+%Ac = b;
+
+
+%% b) Find least-square solution,
+c = A\b
+print c1
+print c2
+
+
+
+
