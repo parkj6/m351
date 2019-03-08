fe42b21cb087eba511c989d46a20dd461d6f832a
Jong Park <parkj6@oregonstate.edu>
2019-03-08 02:25:12 -0800

    HW4 done

--- hw4/hw4.m
+++ hw4/hw4.m
@@ -7,47 +7,48 @@ y = [1, 0.5, 0.4, 0.25, 0.1, 0.1, 0.05, 0.02, 0.01];
 m = length(y);
 x = 1:m;
 
-%plot (x,y)
-
 %% 1. p(x) = c_1e^(-0.5x) + c_2e^(-x); c1, c2 are constants. 
-%% a)  Use appx relation to derive an overdetermined system
-
-%Ac = b;
-
+%% a) Use appx relation to derive an overdetermined system
+       
+u = exp(-0.5.*x(:));         % e^(-0.5x)
+v = exp(-x(:));              % e^(-x)
 
 %% b) Find least-square solution
-c = A\b
-print c1
-print c2
+A = [u,v]
+b = y(:)
+c = A\b                     % Ac = b;
 
-%% c) Plot p(x) with c1, c2 from b. 
+%% c) Plot p(x) with c1, c2. 
 
-% also plot (x1,y1) ,..., (xm,ym) with small circles.
+%x = graphable x from 0:10
+xp = linspace(0,10);
+%p(x) = c_1e^(-0.5x) + c_2e^(-x)
+yp = c(1).*exp(-0.5.*xp) + c(2).*exp(-xp);
 
+plot (xp, yp,'k', x, y,'ok');
+title(sprintf("Approximation relation p(x) = c_1e^{-0.5x} + c_2e^{-x}"))
+legend('p(x)','Initial Data','location','best')
+ax = gca; ax.FontSize = 14; 
 
 %% 2. p(x) = re^(sx); r, s are constants.
 %% a) Use log to break re^(sx_i) and use the result to derive overdetermined system.
 
-%Ac = b;
-% A = matrix
-% b = vector
-% c = vector of unknows.
+u = ones(m,1);
+v = x(:);
+A = [u,v]
+b
 
 %% b) Find least-square solution
+c = A\log(b)            %Ac = b;
 
-print r
-print s
+r = exp(c(1))
+s = c(2)
 
 %% c) Plot
-
-
-%% Notes
-x(:)  % will always turn x as column vector
-
-% given x_1, ..., x_m
-exp(-1(:))  % produces columnvectors with e^(-x_1), ..., e^(-x_m)
-
-ones (m,1)  % produces m x 1 array with all entries = 1. 
-
-
+figure
+yp = r.*exp(s.*xp);
+plot (xp,yp,'k', x,y,'ok')
+title(sprintf("Approximation relation p(x) = re^{sx}"))
+legend('p(x)','Initial Data','location','best')
+ax = gca; ax.FontSize = 14; 
 
